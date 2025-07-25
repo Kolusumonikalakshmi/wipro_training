31b0f85c54215deffc6ab5094a434b153d4a4c66
Kolusumonikalakshmi <monikalakshmik2003@gmail.com>
2025-07-25 21:22:19 +0530

    commit four

---
+++ src/wipro_training2/MultipleInterface_Default.java
@@ -0,0 +1,28 @@
+package wipro_training2;
+interface Interface_A{
+	default void show() {
+		System.out.println("This is the current Show_A");
+	}
+}
+
+interface Interface_B {
+	default void show() {
+		System.out.println("This is the current Show_B");
+	}
+}
+class MyClass implements Interface_A,Interface_B{
+	@Override
+public void show() {
+		Interface_B.super.show();
+		//Interface_A.super.show();
+
+	}
+}
+public class MultipleInterface_Default {
+	public static void main(String[] args) {
+		MyClass c1=new MyClass();
+		c1.show();
+	}
+	
+
+}

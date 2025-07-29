ecfeb4a27b3460918d4d1dfad72f935dfe7aa724
Kolusumonikalakshmi <monikalakshmik2003@gmail.com>
2025-07-29 15:07:18 +0530

    commit seven

---
+++ src/day8/Wrapper_2.java
@@ -0,0 +1,9 @@
+package day8;
+public class Wrapper_2 {
+	  public static void main(String[] args) {
+		   String s = "55";
+		   int i = Integer.parseInt(s); // converting string to integer using parseInt().
+		   System.out.print(i);
+	  }
+	}
+
